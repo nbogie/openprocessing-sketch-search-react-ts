432f35251f68c986700f8111458b27c9ed6a29c8
nbogie <neillbogie@googlemail.com>
2026-03-27 05:48:34 +0000

    tidy mode-selectors code

--- src/ModeSelectors.tsx
+++ src/ModeSelectors.tsx
@@ -12,45 +12,54 @@ export function ModeSelectors({
 }): JSX.Element {
     return (
         <>
-            <label>
-                P5
-                <input
-                    type="checkbox"
-                    checked={includeModes.p5js}
-                    onChange={() =>
-                        setIncludeModes((prev) => ({
-                            ...prev,
-                            p5js: !prev.p5js,
-                        }))
-                    }
-                />
-            </label>
-            <label>
-                html
-                <input
-                    type="checkbox"
-                    checked={includeModes.html}
-                    onChange={() =>
-                        setIncludeModes((prev) => ({
-                            ...prev,
-                            html: !prev.html,
-                        }))
-                    }
-                />
-            </label>
-            <label>
-                (legacy) Pjs
-                <input
-                    type="checkbox"
-                    checked={includeModes.pjs}
-                    onChange={() =>
-                        setIncludeModes((prev) => ({
-                            ...prev,
-                            pjs: !prev.pjs,
-                        }))
-                    }
-                />
-            </label>
+            <ModeSelector
+                label={"P5"}
+                keyVal={"p5js"}
+                includeModes={includeModes}
+                setIncludeModes={setIncludeModes}
+            />
+            <ModeSelector
+                label={"html"}
+                keyVal={"html"}
+                includeModes={includeModes}
+                setIncludeModes={setIncludeModes}
+            />
+            <ModeSelector
+                label={"(legacy) Pjs"}
+                keyVal={"pjs"}
+                includeModes={includeModes}
+                setIncludeModes={setIncludeModes}
+            />
         </>
+    );
+}
+
+function ModeSelector({
+    label,
+    keyVal,
+    includeModes,
+    setIncludeModes,
+}: {
+    label: string;
+    keyVal: OPSketchMode;
+    includeModes: Record<OPSketchMode, boolean>;
+    setIncludeModes: React.Dispatch<
+        React.SetStateAction<Record<OPSketchMode, boolean>>
+    >;
+}) {
+    return (
+        <label>
+            {label}
+            <input
+                type="checkbox"
+                checked={includeModes[keyVal]}
+                onChange={() =>
+                    setIncludeModes((prev) => ({
+                        ...prev,
+                        [keyVal]: !prev[keyVal],
+                    }))
+                }
+            />
+        </label>
     );
 }
